fbca35b0fbd791199c136a0f0bf7e55b2e9d3c85
tanvir <mdtanvirislams@gmail.com>
2020-05-15 05:08:26 +0600

    input and outputting a list of items(hooks)

---
+++ App.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+
+export default function App() {
+  const [enteredGoal, setEnteredGoal]= useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+    setCourseGoals(currentGoals => [...courseGoals, enteredGoal]);
+  }
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Course Goal"
+            style={styles.input}
+            onChangeText={goalInputHandler} 
+            value={enteredGoal}
+          />
+          <Button title="ADD" onPress={addGoalHandler}/>
+      </View>
+      <ScrollView>
+          {courseGoals.map((goal) => <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 15,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
+
+
