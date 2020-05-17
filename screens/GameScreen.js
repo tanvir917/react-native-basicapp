01f89631fdd28eb017c7ebff115e91e3ce1e28e8
tanvir <mdtanvirislams@gmail.com>
2020-05-17 19:12:17 +0600

    using flatlist

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -19,17 +19,17 @@ const generateRandomBetween = (min, max, exclude) => {
     }
 }
 
-const renderListItem = (value, numOfRounds) => (
-    <View key={value} style={styles.listItem}>
-        <BodyText>#{numOfRounds}</BodyText>
-        <BodyText>{value}</BodyText>
+const renderListItem = (listLength, itemData) => (
+    <View style={styles.listItem}>
+        <BodyText>#{listLength - itemData.index}</BodyText>
+        <BodyText>{itemData.item}</BodyText>
     </View>
 )
 
 const GameScreen = props => {
     const initialGuess = generateRandomBetween(1, 100, props.userChoice)
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
-    const [pastGuesses, setPastGuesses] = useState([initialGuess]);
+    const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
 
@@ -58,7 +58,7 @@ const GameScreen = props => {
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
         setCurrentGuess(nextNumber);
         //setRounds(curRounds => curRounds + 1)
-        setPastGuesses(curPastGuesses => [nextNumber , ...curPastGuesses])
+        setPastGuesses(curPastGuesses => [nextNumber.toString() , ...curPastGuesses])
     }
 
     return (
@@ -74,9 +74,15 @@ const GameScreen = props => {
                 </MainButton>
             </Card>
             <View style={styles.listContainer}>
-                <ScrollView contentContainerStyle={styles.list}>
+                {/*<ScrollView contentContainerStyle={styles.list}>
                     {pastGuesses.map((guess, index )=> renderListItem(guess, pastGuesses.length - index))}
-                </ScrollView>
+                </ScrollView>*/}
+                <FlatList 
+                    keyExtractor={(item) => item} 
+                    data={pastGuesses} 
+                    renderItem={renderListItem.bind(null, pastGuesses.length)}
+                    contentContainerStyle={styles.list}
+                />
             </View>
         </View>
     )
